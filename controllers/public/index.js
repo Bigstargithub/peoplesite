d847b0ee61fb72c73c35c205c6fa403b49fad71c
Bigstargithub <37678862+Bigstargithub@users.noreply.github.com>
2021-07-27 17:55:34 +0900

    가인지피플 9일차

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -4,4 +4,6 @@ const ctrl = require('./public.ctrl');
 
 router.get('/', ctrl.get_main);
 
+router.get('/notice/:id', ctrl.get_notice_detail);
+
 module.exports = router;
--- controllers/public/public.ctrl.js
+++ controllers/public/public.ctrl.js
@@ -9,3 +9,33 @@ exports.get_main = async (req, res) => {
         notice_list,
     });
 }
+
+exports.get_notice_detail = async( req, res) => {
+    const id = req.params.id;
+    const notice = await models.notice_list.findOne({ where: {number: id}});
+    if(notice)
+    {
+        var main_work = notice.main_work.split(',');
+        var job_qualify = notice.job_qualify.split(',');
+        var job_prefer = notice.job_prefer.split(',');
+        var job_welfare = notice.job_welfare.split(',');
+        var company_communication = notice.company_communication.split(',');
+        var company_culture = notice.company_culture.split(',');
+        var company_people = notice.company_people.split(',');
+        var next_career = notice.next_career.split(',');
+        var notice_process = notice.notice_process.split(',');
+
+        res.render('notice_detail',{
+            notice,
+            main_work,
+            job_qualify,
+            job_prefer,
+            job_welfare,
+            company_communication,
+            company_culture,
+            company_people,
+            next_career,
+            notice_process,
+        });
+    }
+}
