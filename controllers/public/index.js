c7005c8e98f8b5eab83dfdbff204d273020c1b1e
Bigstargithub <37678862+Bigstargithub@users.noreply.github.com>
2021-07-28 21:07:49 +0900

    가인지피플 10일차

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -4,6 +4,10 @@ const ctrl = require('./public.ctrl');
 
 router.get('/', ctrl.get_main);
 
+router.get('/:group', ctrl.get_main_group);
+
+router.get('/apply/:id', ctrl.get_apply);
+
 router.get('/notice/:id', ctrl.get_notice_detail);
 
 module.exports = router;
--- controllers/public/public.ctrl.js
+++ controllers/public/public.ctrl.js
@@ -10,19 +10,31 @@ exports.get_main = async (req, res) => {
     });
 }
 
+exports.get_main_group = async(req, res) => {
+    const group = req.params.group;
+    const notice_list = await models.notice_list.findAll({ where: {job_group: group}});
+    if(notice_list)
+    {
+        res.render('main',{
+            notice_list,
+            group,
+        })
+    }
+}
+
 exports.get_notice_detail = async( req, res) => {
     const id = req.params.id;
     const notice = await models.notice_list.findOne({ where: {number: id}});
     if(notice)
     {
-        var main_work = notice.main_work.split(',');
-        var job_qualify = notice.job_qualify.split(',');
-        var job_prefer = notice.job_prefer.split(',');
-        var job_welfare = notice.job_welfare.split(',');
-        var company_communication = notice.company_communication.split(',');
-        var company_culture = notice.company_culture.split(',');
-        var company_people = notice.company_people.split(',');
-        var next_career = notice.next_career.split(',');
+        var main_work = notice.main_work.split('\n');
+        var job_qualify = notice.job_qualify.split('\n');
+        var job_prefer = notice.job_prefer.split('\n');
+        var job_welfare = notice.job_welfare.split('\n');
+        var company_communication = notice.company_communication.split('\n');
+        var company_culture = notice.company_culture.split('\n');
+        var company_people = notice.company_people.split('\n');
+        var next_career = notice.next_career.split('\n');
         var notice_process = notice.notice_process.split(',');
 
         res.render('notice_detail',{
@@ -38,4 +50,11 @@ exports.get_notice_detail = async( req, res) => {
             notice_process,
         });
     }
+}
+
+exports.get_apply = async (req, res) => {
+    const id = req.params.id;
+    res.render('reg_notice', {
+        id,
+    });
 }
